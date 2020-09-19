b6544e5915aeb6c6da0275e2d21aa702ff3060ac
Leonardo Felipe dos Santos <42083917+LeonardoFdSantos@users.noreply.github.com>
2020-09-19 15:04:51 -0300

    metodos de conexão do PYTHON

--- query_fetchmany.py
+++ query_fetchmany.py
@@ -1,19 +1,24 @@
 from mysql.connector import MySQLConnection, Error
 from python_mysql_dbconfig import read_db_config
 
+def iter_row(cursor, size=10):
+    while True:
+        rows = cursor.fetchmany(size)
+        if not rows:
+            break
+        for row in rows:
+            yield row
 
-def query_with_fetchall():
+def query_with_fetchmany():
     global cursor, conn
     try:
         dbconfig = read_db_config()
         conn = MySQLConnection(**dbconfig)
         cursor = conn.cursor()
+
         cursor.execute("SELECT * FROM consolidados")
 
-        rows = cursor.fetchall()
-
-        print('Total Row(s):', cursor.rowcount)
-        for row in rows:
+        for row in iter_row(cursor, 10):
             print(row)
 
     except Error as e:
@@ -25,4 +30,4 @@ def query_with_fetchall():
 
 
 if __name__ == '__main__':
-    query_with_fetchall()
+    query_with_fetchmany()
--- query_fetchone.py
+++ query_fetchone.py
@@ -2,6 +2,7 @@ from mysql.connector import MySQLConnection, Error
 from python_mysql_dbconfig import read_db_config
 
 def query_with_fetchone():
+    global cursor, conn
     try:
         dbconfig = read_db_config()
         conn = MySQLConnection(**dbconfig)
